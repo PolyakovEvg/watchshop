d8720e576577ef5f32bc21a52f08af6985a58110
Евгений Поляков <epoliakov@gmail.com>
2023-02-02 22:33:19 +0300

    fix bugs12

--- bin/render_build.sh
+++ bin/render_build.sh
@@ -1,9 +1,9 @@
 set -o errexit
 
 bundle install
+bundle exec rake assets:precompile
 yarn install
 yarn build:css
 yarn build
-bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
